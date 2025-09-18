58f4d7b56af672d633e757134caf2e068c58ca76
iTSCarlXanders <xanderdayrit6@gmail.com>
2025-09-18 08:49:39 +0800

    Add item class for composition

--- hero.js
+++ hero.js
@@ -5,10 +5,19 @@ class Hero {
         this.name = name;
         this.#health = health;
         this.attack = attack;
+        this.items = [];
     }
 
     getHealth() { 
         return this.#health;
+    }
+
+    getAttack(){
+        console.log(this.attack);
+
+    }
+    getName(){
+        console.log(this.name);
     }
 
     getStats(){
@@ -16,6 +25,14 @@ class Hero {
         console.log("Name:  " +this.name);
         console.log("Health: "+ this.#health);
         console.log("Attack: "+ this.attack);
+    }
+
+    addItem(item){
+        this.items.push(item);
+    }
+
+    totalAttack(){
+        return this.attack + this.items.reduce((sum,i)=>sum+i.bonusAttack, 0);
     }
 }
 
@@ -29,30 +46,51 @@ class Mage extends Hero {
     constructor(name, health, attack, mana) {
         super(name, health, attack);
         this.mana = mana;
-    }
-
-    
+    } 
     useAbility() {
         console.log(`${this.name} casts FireBaaAaaaALL!`);
     }
 }
-   function performAbility(hero){
-    console.log("\n");
-        hero.useAbility();
-    }
+  
+class Item{
 
-const Thorin = new Warrior("Thorin", 100, 10);
+    constructor(name,bonusAttack){
+        this.name = name;
+        this.bonusAttack = bonusAttack;
+    }
+}
+
+function performAbility(hero){
+    console.log("\n");
+    hero.useAbility();
+}
+    const sword = new Item("Sword", 5);
+    const staff = new Item("Staff", 3);
+       
+    const Thorin = new Warrior("Thorin",100,10); 
+        
+    Thorin.getName();
+    Thorin.getAttack();
+    Thorin.addItem(sword);
+    console.log(Thorin.totalAttack());
+     Thorin.addItem(sword);
+    console.log(Thorin.totalAttack());
+     Thorin.addItem(sword);
+    console.log(Thorin.totalAttack());  
+     Thorin.addItem(sword);
+    console.log(Thorin.totalAttack());
+
+
+
+//const Thorin = new Warrior("Thorin", 100, 10);
 //Thorin.useAbility(); 
-Thorin.getStats();
+//Thorin.getStats();
 
-const Gandalf = new Mage("Gandalf", 80, 8, 50);
+//const Gandalf = new Mage("Gandalf", 80, 8, 50);
 //Gandalf.useAbility();
-Gandalf.getStats();
+//Gandalf.getStats();
 
- function performAbility(hero){
-    console.log("\n");
-        hero.useAbility();
-    }
+ 
 
-    performAbility(Thorin);
-    performAbility(Gandalf);
+//performAbility(Thorin);
+//performAbility(Gandalf);
